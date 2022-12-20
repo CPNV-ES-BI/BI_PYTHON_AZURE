fdc5c2d429e6ed2daadb2403f947ecc6eeb4904b
melohan <melodie.ohan@cpnv.ch>
2022-12-20 10:36:48 +0100

    Delete DataObject custom exceptions

--- src/exceptions/data_object_already_exists.py
+++
@@ -1,5 +0,0 @@
-from .data_object_exception import DataObjectException
-
-
-class DataObjectAlreadyExists(DataObjectException):
-    "Occurs when a DataObject alrady exists"
--- src/exceptions/data_object_does_not_exist.py
+++
@@ -1,5 +0,0 @@
-from .data_object_exception import DataObjectException
-
-
-class DataObjectDoesNotExist(DataObjectException):
-    "Occurs when an operation is done on an object that does not exist "
