ede9bb8f376fc7c48df5aa5aba9b883931787878
melohan <melodie.ohan@cpnv.ch>
2022-12-20 11:33:06 +0100

    Edit directory test path

--- run_unittests.sh
+++ run_unittests.sh
@@ -6,5 +6,6 @@ BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "Adding \`src\` directory to the PYTHONPATH.."
 export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/src/"
 
+# TODO make it recursively find all sub-directories
 echo "Running unit tests.."
-python3 -m unittest discover tests/
+python3 -m unittest discover tests/blob
