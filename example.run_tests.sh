a3291f0d8577a29290911cc2655df5f9423cbb02
melohan <melodie.ohan@cpnv.ch>
2022-12-21 08:51:13 +0100

    Rename run_unittests and provide an example file

--- example.run_tests.sh
+++ example.run_tests.sh
@@ -3,8 +3,15 @@
 # Get the project directory
 BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# Add src to python path
 echo "Adding \`src\` directory to the PYTHONPATH.."
 export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/src/"
+
+# Set env variables
+# TODO remove env variables definition from this file
+echo "Set env variables.."
+export AZURE_STORAGE_CONNECTION_STRING=""
+export AZURE_CONTAINER_NAME=""
 
 # TODO make it recursively find all sub-directories
 echo "Running unit tests.."
