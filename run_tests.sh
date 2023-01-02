a9d440c22dda7dad0655ea41cfaf6869d51eb44c
melohan <melodie.ohan@cpnv.ch>
2023-01-02 16:01:58 +0100

    Remove settings from run_tests script and create a setting_up_dev script

--- run_tests.sh
+++ run_tests.sh
@@ -7,10 +7,6 @@ BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "Adding \`src\` directory to the PYTHONPATH.."
 export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/src/"
 
-# Set env variables
-echo "Set env variables.."
-source env.variables.sh
-
 # Run any tests in `tests` directory
 echo "Running unit tests.."
 python3 -m unittest discover -s tests/blob
---
+++ setting_up_dev.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# venv activation function
+# It does not display the venv but it works
+# Check with `which pip` command before and after the execution
+activate () {
+    source .venv/bin/activate
+}
+
+# Get the project directory
+BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Add `src` to python path
+echo "Adding \`src\` directory to the PYTHONPATH.."
+export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/src/"
+
+# Copy the env variables file
+echo "Create the configuration file.."
+cp .env.variables.sh env.variables.sh
+chmod +x env.variables.sh
+
+# Export the environment variables
+export VIRTUAL_ENV_DISABLE_PROMPT=0
+
+# venv Creation
+echo "Create the virutal environment.."
+python3 -m venv .venv
+
+# venv Activation
+echo "Activate the virutal environment.."
+chmod +x .venv/bin/activate
+activate
+
+# Install dev requirements.txt
+echo "Install the requirements to the venv $(which pip)"
+pip install -r requirements.txt
+
+echo "Please define your connection string in ./src/env.variables.sh"
