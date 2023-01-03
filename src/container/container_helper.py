73d1c403c8c00220024a22ebb8961975bc650215
melohan <melodie.ohan@cpnv.ch>
2023-01-03 16:37:53 +0100

    Edit does_exist() method

--- src/container/container_helper.py
+++ src/container/container_helper.py
@@ -16,20 +16,10 @@ class ContainerHelper:
         self._storage_client = storage_client
 
     def does_exist(self, object_name: str) -> bool:
-        containers: list = self._storage_client.blob_service_client.list_containers(object_name)
-        for container in containers:
-            if container.name == object_name:
-                return True
-        return False
+        return self._storage_client.get_container_client(object_name).exists()
 
     def create(self, object_name: str) -> None:
         self._storage_client.blob_service_client.create_container(object_name)
-
-    def download(self, object_path: str) -> str:
-        pass
-
-    def publish(self, object_path: str) -> None:
-        pass
     
     def delete(self, object_name: str) -> None:
         self._storage_client.blob_service_client.delete_container(object_name)
