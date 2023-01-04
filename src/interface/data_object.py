bfa1700fec9f22075772a58e1285baff060420c4
melohan <melodie.ohan@cpnv.ch>
2023-01-04 16:32:33 +0100

    Edit methods arguments and documentation, add delete method

--- src/interface/data_object.py
+++ src/interface/data_object.py
@@ -14,16 +14,15 @@ class DataObject:
     All methods must be implemented in inheriting class.
     """
 
-    def does_exist(self, object_path: str) -> bool:
+    def does_exist(self, object_name: str) -> bool:
         """Check if the data object exists in the distant storage"""
         raise NotImplementedError("Not implemented!")
 
-    def create(self, object_path: str, local_file_path: str) -> None:
+    def create(self, object_name: str, local_file_path: str) -> None:
         """Create the data object in the distant storage
 
         Args:
-            object_path: str       Path to the object in the distant storage
-                                   Ex: `subdir1/subdir2/myfile.txt" 
+            object_name: str       Data object name
 
             local_file_path: str   Local path (path + file.extension)
 
@@ -38,11 +37,13 @@ class DataObject:
         """
         raise NotImplementedError("Not implemented!")
 
-    def download(self, object_path: str) -> str:
+    def download(self, object_name: str, local_file_path: str) -> str:
         """Download the file from the distant data object through its name
 
         Args:
-            object_path: str       Data object name (file.extension)
+            object_name: str       Data object name
+            local_file_path: str   Local path (full path to the downloaded object)
+
 
         Returns: 
             Path to the downloaded object 
@@ -53,15 +54,24 @@ class DataObject:
         """
         raise NotImplementedError("Not implemented!")
 
-    def publish(self, object_path: str) -> None:
+    def publish(self, object_name: str) -> None:
         """Set the data_object public
 
         Args:
-            object_path: str       Data object name (file.extension)
+            object_name: str       Data object name (file.extension)
             local_file_path: str   Local path (path + file.extension)
 
         Raises: 
             Exception the data object name does not exists
-        
         """
         raise NotImplementedError("Not implemented!")
+
+    def delete(self, object_name: str) -> None:
+        """Delete the data object by its name
+
+        Args:
+            object_name: str       Data object name (file.extension)
+
+        Raises: 
+            Exception the data object name does not exists
+        """
