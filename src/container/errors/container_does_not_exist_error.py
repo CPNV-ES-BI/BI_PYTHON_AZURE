013e80b8ea0d16fe63bdd695b9f92c5b9a4d02fc
melohan <melodie.ohan@cpnv.ch>
2023-01-22 11:40:40 +0100

    Rename container custom exception

--- src/container/errors/container_does_not_exist_error.py
+++ src/container/errors/container_does_not_exist_error.py
@@ -1,6 +1,6 @@
 from container.errors.container_error import ContainerError
 
 
-class BlobDoesNotExistError(ContainerError):
+class ContainerDoesNotExistError(ContainerError):
     """Raised when trying to perform an operation on a container that does not exist"""
     pass
