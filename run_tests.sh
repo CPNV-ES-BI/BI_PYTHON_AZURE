6437ac4dc473927c4bdd62f98ea3eb656f2eac02
melohan <melodie.ohan@cpnv.ch>
2023-01-22 22:39:17 +0100

    Make the script searches and finds each unit test

--- run_tests.sh
+++ run_tests.sh
@@ -6,9 +6,7 @@ BASEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Add `src` to python path
 echo "Adding \`src\` and \`tests\` directories to the PYTHONPATH.."
 export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/src/"
-export PYTHONPATH="${PYTHONPATH}:${BASEDIR}/"
 
 # Run any tests in `tests` directory
 echo "Running unit tests.."
-python3 -m unittest discover -s tests/blob/
-python3 -m unittest discover -s tests/container/
+python3 -m unittest discover
