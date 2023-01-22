69220bc456ddd4ebd3c7c396c28f00c8395514e4
melohan <melodie.ohan@cpnv.ch>
2023-01-22 12:34:11 +0100

    Apply signatures changes on container and implement custom exceptions.
    This commit makes major changes:
    - downloading several blobs simultaneously is no longer possible
    - Operations that are not possible are handled by appropriate exceptions

--- src/container/container.py
+++ src/container/container.py
@@ -6,12 +6,13 @@
 # -----------------------------------------------------------------------------------
 
 from interface.data_object import DataObject
-from src.config.azure_client import AzureClient
-from src.blob.blob import Blob   
+from config.azure_client import AzureClient
+from container.errors.container_already_exists_error import ContainerAlreadyExistsError
+from container.errors.container_does_not_exist_error import ContainerDoesNotExistError
+from container.errors.container_forbidden_operation_error import ContainerForbiddenOperationError
 
 
 class Container(DataObject):
-
     _storage_client: AzureClient
 
     def __init__(self, storage_client: AzureClient) -> None:
@@ -19,31 +20,31 @@ class Container(DataObject):
             raise ValueError("Missing parameter: AzureClient")
         self._storage_client = storage_client
 
+    # close the connection
+    def __del__(self):
+        self._storage_client.close_connection()
+
     def does_exist(self, container_name: str) -> bool:
         return self._storage_client.get_container_client(container_name).exists()
 
-    def create(self, container_name: str) -> None:
+    def create(self, container_name: str, local_file_path: str = None) -> None:
+        if local_file_path is not None:
+            raise ContainerForbiddenOperationError("Container creation is not based on a file path.")
         if self.does_exist(container_name):
-             raise Exception(f"container `{container_name}` already exists.")
+            raise ContainerAlreadyExistsError(f"Container `{container_name}` already exists.")
         self._storage_client.get_blob_service_client().create_container(container_name)
 
     def publish(self, container_name: str) -> str:
-        if not self.does_exist(container_name):
-            raise Exception(f"container `{container_name}` does not exist.")
-        raise NotImplementedError("It is not possible yet to define a public container.")     
+        raise ContainerForbiddenOperationError("This operation is not possible on container.")
+        return None
 
-    def download(self, container_name: str) -> list[bytes]:
-        if not self.does_exist(container_name):
-            raise Exception(f"container `{container_name}` does not exist.")
-        blobs: list = self._storage_client.get_container_client(container_name).list_blobs()
-        result = list[bytes]()
-        for blob in blobs:
-            data: list[bytes] = Blob(self._storage_client, container_name).download(blob.name)
-            if data:
-                result.append(data[0])
-        return result
+    def download(self, container_name: str) -> bytes:
+        raise ContainerForbiddenOperationError("This operation is not possible on container")
+        return None
 
-    def delete(self, container_name: str, recusrive: bool = True) -> None:
+    def delete(self, container_name: str, recursive: bool = True) -> None:
+        if not recursive:
+            raise ContainerForbiddenOperationError("Deleting a container means deleting all its contents")
         if not self.does_exist(container_name):
-            raise Exception(f"container `{container_name}` does not exist.")
+            raise ContainerDoesNotExistError(f"Container `{container_name}` does not exist.")
         self._storage_client.get_blob_service_client().delete_container(container_name)
--- tests/container/test_container.py
+++ tests/container/test_container.py
@@ -1,58 +1,62 @@
 import unittest
+import uuid
 
-from tests.functions import get_random_name, create_test_directory, delete_directory, merge_paths
 from config.azure_client import AzureClient
 from container.container import Container
-from blob.blob import Blob
+from container.errors.container_already_exists_error import ContainerAlreadyExistsError
+from container.errors.container_does_not_exist_error import ContainerDoesNotExistError
+from container.errors.container_forbidden_operation_error import ContainerForbiddenOperationError
 
 
 class TestContainer(unittest.TestCase):
 
     # Attributes area
     # -----------------------------------------------------------------------    
+    # class attributes
+    __storage_client: AzureClient
 
     # instance attributes
-    __container: Container 
-    __container_name: str   
-
-    # Class attributes
-    __storage_client: AzureClient     
-    __blob_name: str
-    __local_file_path: str                
+    __container: Container
+    __container_name: str
 
     # Test class methods area
     # -----------------------------------------------------------------------
-  
-    @classmethod
-    def __create_blob(cls, container_name: str):
-        cls.__blob = Blob(TestContainer.__storage_client, container_name)
-        cls.__blob_name = "TestContainer/example.txt"
-        cls.__blob.create(cls.__blob_name, TestContainer.__local_file_path)
-    
+    @staticmethod
+    def get_random_container_name() -> str:
+        """ Return a unique container name.
+        The prefix of the container is "container-" and it is completed with 15 generated char from uuid.
+
+        :return: str
+
+        """
+        # Use the first 15 characters and the last 15 characters of the UUID to limit collision in names
+        uuid_str = str(uuid.uuid4()).replace("-", "")
+        return f"container-{uuid_str[:15] + uuid_str[-15:]}"
+
+    # incoming. ..
+
     # SetUp and TearDown area
     # -----------------------------------------------------------------------
 
     # Before all
     @classmethod
     def setUpClass(cls):
+        # Will be defined for every class
         cls.__storage_client = AzureClient()
-        # Creates tests environnment
-        file_name = f"{get_random_name()}.txt"
-        cls.__tmp_dir = create_test_directory(file_name)
-        cls.__local_file_path = merge_paths(cls.__tmp_dir, file_name)
 
     # After all
     @classmethod
     def tearDownClass(cls):
-        delete_directory(cls.__tmp_dir)
+        # AzureClient and Container instance are properly collected by the GC
+        # Container close himself any connection
+        pass
 
     # Before each
     def setUp(self):
-        # create a container with a blob
-        self.__container_name = get_random_name() 
-        self.__container = Container(TestContainer.__storage_client)
+        storage_client = AzureClient()
+        self.__container_name = TestContainer.get_random_container_name()
+        self.__container = Container(storage_client)
         self.__container.create(self.__container_name)
-        TestContainer.__create_blob(self.__container_name)
 
     # After each 
     def tearDown(self):
@@ -65,89 +69,107 @@ class TestContainer(unittest.TestCase):
 
     def test_does_exist_exists_case_true(self):
         # given
-        # refer to setUpClass and setUp()
+        # refer to setUpClass and setUp() and setUpClass()
 
         # when
         result: bool = self.__container.does_exist(self.__container_name)
-        
+
         # then
         self.assertEqual(result, True)
 
     def test_does_exist_not_exists_false(self):
-        # given 
-        # refer to and setUp()
-        container_name = get_random_name() 
-
+        # given
+        # refer to setUpClass and setUp() and setUpClass()
+        container_name = TestContainer.get_random_container_name()
         # when
         result: bool = self.__container.does_exist(container_name)
-        
+
         # then
         self.assertEqual(result, False)
 
     # depends on test_does_exist_exists_case_success
     def test_create_object_nominal_case_object_exists(self):
-        # given        
+        # given
         # when
-        # refer to setUp()
-    
-        # then  
+        # refer to setUp() and setUpClass()
+
+        # then
         self.assertEqual(self.__container.does_exist(self.__container_name), True)
-    
+
     def test_create_object_already_exists_throw_exception(self):
         # given
         # refer to setUpClass and setUp()
 
         # when
         # then
-        with self.assertRaises(Exception):
+        with self.assertRaises(ContainerAlreadyExistsError):
             self.__container.create(self.__container_name)
 
-    def test_download_object_nominal_case_downloaded(self):
+    # Depends on test_does_exist_exists_case_success
+    # There is no path verification, containers creation do not depend on it.
+    def test_create_object_path_not_exists_object_exists(self):
+        # given
+        # when
+        # refer to setUp() and setUpClass()
+
+        # then
+        self.assertEqual(self.__container.does_exist(self.__container_name), True)
+
+    # There is no path verification, containers creation do not depend on it.
+    def test_create_object_path_is_not_none_object_throw_exception(self):
+        # given
+        # when
+        # refer to setUp() and setUpClass()
+        path: str = "any/path"
+
+        # then
+        with self.assertRaises(ContainerForbiddenOperationError):
+            self.__container.create(self.__container_name, path)
+
+    def test_download_object_nominal_case_throw_exception(self):
         # given
         # refer to setUpClass and setUp()
 
         # when
-        result = self.__container.download(self.__container_name)
 
         # then
         # It checks the full path to the blob
-        self.assertIsNotNone(result)
+        with self.assertRaises(ContainerForbiddenOperationError):
+            self.__container.download(self.__container_name)
 
     def test_download_object_not_exists_throw_exception(self):
         # given
         # refer to setUpClass and setUp()
-        container_name = get_random_name()
+        container_name = TestContainer.get_random_container_name()
 
         # when
         # then
-        with self.assertRaises(Exception):
+        with self.assertRaises(ContainerForbiddenOperationError):
             self.__container.download(container_name)
 
-    def test_publish_object_nominal_case_object_published(self):
+    def test_publish_object_nominal_case_throw_exception(self):
         # given
         # refer to setUpClass and setUp()
 
         # when
-        result: str = self.__container.publish(self.__container_name)
-
         # then
-        # should return an url
-        self.assertIsNotNone(result)
+        with self.assertRaises(ContainerForbiddenOperationError):
+            self.__container.publish(self.__container_name)
 
     def test_publish_object_object_not_found_throw_exception(self):
         # given
         # refer to setUpClass and setUp()
-        container_name: str = get_random_name()
+        container_name = TestContainer.get_random_container_name()
 
         # when
         # then
-        with self.assertRaises(Exception):
-            self.__container.publish(self.__container_name)
+        with self.assertRaises(ContainerForbiddenOperationError):
+            self.__container.publish(container_name)
 
-    # depends on test_does_exist_not_exists_false
+    # depends on test_does_exist_exists_case_success
     def test_delete_object_object_exists_object_deleted(self):
         # given
-        # refer to setUp()
+        # refer to setUpClass and setUp()
 
         # when
         self.__container.delete(self.__container_name)
@@ -158,24 +180,34 @@ class TestContainer(unittest.TestCase):
     # depends on test_does_exist_not_exists_false
     def test_delete_object_object_containing_sub_objects_exists_object_deleted_recursively(self):
         # given
-        # refer to setUp()
-        
+        # refer to setUpClass and setUp()
+
         # when
         self.__container.delete(self.__container_name)
 
         # then
-        # We assume that the delete method delete any blob contained in the container
         self.assertEqual(self.__container.does_exist(self.__container_name), False)
+
+    def test_delete_object_object_not_recursive_deletion_throw_exception(self):
+        # given
+        # refer to setUpClass and setUp()
+        recursive: bool = False
+
+        # when
+        # then
+        with self.assertRaises(ContainerForbiddenOperationError):
+            self.__container.delete(self.__container_name, recursive)
 
     def test_delete_object_object_doesnt_exist_throw_exception(self):
         # given
         # refer to setUp()
-        container_name = get_random_name()
+        container_name = TestContainer.get_random_container_name()
 
         # when
         # then
-        with self.assertRaises(Exception):
+        with self.assertRaises(ContainerDoesNotExistError):
             self.__container.delete(container_name)
+
 
 if __name__ == '__main__':
     unittest.main()
