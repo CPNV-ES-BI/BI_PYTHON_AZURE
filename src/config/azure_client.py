19294d3777c8b122c8c2cf089c5c12234360ca1c
melohan <melodie.ohan@cpnv.ch>
2023-01-22 17:44:54 +0100

    Make AzureClient closing connection to the BlobServiceClient

--- src/config/azure_client.py
+++ src/config/azure_client.py
@@ -1,18 +1,17 @@
-# -----------------------------------------------------------------------------------  
-# File   :   client.py
-# Author :   Mélodie Ohan
-# Version:   14-01-2023 - original (dedicated to BI1)
-# Remarks:   -
-# -----------------------------------------------------------------------------------
-
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from src.config.azure_config import AzureConfig
 
+
 class AzureClient:
-    """Provide the appropriate client based on AzureConfig"""
+    """Provide the appropriate client based on AzureConfig
+
+    Notes
+    ---
+    Manages the closing of the connection when the instance is destroyed
+
+    """
 
     __service_client: BlobServiceClient
-    
 
     def __init__(self) -> None:
         """Constructor
@@ -22,7 +21,15 @@ class AzureClient:
         """
         self._config = AzureConfig()
         self.__service_client = BlobServiceClient.from_connection_string(self._config.connection_string)
-    
+
+    def __del__(self):
+        """ Destructor
+
+         Close the connection to the BlobServiceClient
+
+        """
+        self.__service_client.close()
+
     def get_blob_service_client(self) -> BlobServiceClient:
         return self.__service_client
     
@@ -30,4 +37,4 @@ class AzureClient:
         return self.__service_client.get_container_client(container_name)
 
     def get_blob_client(self, container_name: str, blob_name: str) -> BlobClient:
-        return self.__service_client.get_blob_client(container_name, blob_name)    
+        return self.__service_client.get_blob_client(container_name, blob_name)
